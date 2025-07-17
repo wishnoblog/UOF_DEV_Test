72cde64e2f02ec36c7adedc26db1814a69fd6900
wish <wishnoblog@gmail.com>
2025-07-17 09:35:57 +0800

    Add project files.

---
+++ FCF.TRIGGER_SAMPLE/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using Ede.Uof.WKF.ExternalUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCF.TRIGGER_SAMPLE
+{
+    /// <summary>
+    /// 一個TRIGGER 範例
+    /// </summary>
+    public class Trigger : ICallbackTriggerPlugin
+    {
+    }
+}
