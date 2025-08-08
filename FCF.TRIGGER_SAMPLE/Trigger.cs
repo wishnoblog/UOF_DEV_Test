950b56212915dfc11d181acfbbf58b0bf59578e1
wish <wishnoblog@gmail.com>
2025-08-08 11:14:11 +0800

    加入寫回表單單機制

--- FCF.TRIGGER_SAMPLE/Trigger.cs
+++ FCF.TRIGGER_SAMPLE/Trigger.cs
@@ -44,10 +44,40 @@ namespace FCF.TRIGGER_SAMPLE
             //可以透過 task_id 或 doc_nbr 在資料表 [TB_WKF_TASK] 的[CURRENT_DOC]欄位取得表單內容
             var task = applyTask.Task;
             var task_Id = task.TaskId;
-            Ede.Uof.Utility.Log.Logger.Write("Debug", $"測試表單取到的DOC_NBR是{DOC_NBR} Task Id{task_Id}");
-            
-            //將目前表單內容讀到 doc物件當中
-            XmlDocument doc = new XmlDocument();
+            //Ede.Uof.Utility.Log.Logger.Write("Debug", $"測試表單取到的DOC_NBR是{DOC_NBR} Task Id{task_Id}");
+
+            //處理資料回寫
+            //取得表單內容XML
+            var doc = GetFormDocByTaskID(task_Id);
+            //讀取文件的XML中SCORE欄位，之後轉成int?
+            int? score = TryParseInt(GetFieldValue(doc, "SCORE"));
+            //Ede.Uof.Utility.Log.Logger.Write("Debug", $"測試表單取到的SCORE是{score}");
+
+            if (score.HasValue)
+            {
+                string rank = "D";
+                if (score >= 91)
+                {
+                    rank = "A++";
+                }
+                else if (score > 81)
+                {
+                    rank = "A+";
+                }
+                else if (score > 71)
+                {
+                    rank = "A";
+                }
+                else if (score > 61)
+                {
+                    rank = "B";
+                }
+                //Ede.Uof.Utility.Log.Logger.Write("Debug", $"rank = {rank}");
+                ReplaceDocumentContent(task_Id, "RANK", rank);
+            }
+            //重新讀取異動的表單
+            doc = GetFormDocByTaskID(task_Id);
+            //將目前表單內容讀到 doc物件當中            
             doc.LoadXml(applyTask.CurrentDocXML);
             //將表單內容存一分到硬碟中(上線會註解掉) 用於後續拆解XML
             doc.Save($"D:\\UOF_DEBUG\\{DOC_NBR}{DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss")}.xml");
@@ -223,7 +253,12 @@ namespace FCF.TRIGGER_SAMPLE
         }
 
         #region 簡化XML 取得欄位值的方法
-        // 轉換型別的方法
+        
+        /// <summary>
+        /// 字串轉INT
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         private int? TryParseInt(string value)
         {
             if (int.TryParse(value, out int result))
@@ -231,6 +266,11 @@ namespace FCF.TRIGGER_SAMPLE
             return null;
         }
 
+        /// <summary>
+        /// 字串轉DateTime
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         private DateTime? TryParseDateTime(string value)
         {
             if (DateTime.TryParse(value, out DateTime result))
@@ -238,6 +278,11 @@ namespace FCF.TRIGGER_SAMPLE
             return null;
         }
 
+        /// <summary>
+        /// 字串轉Decimal
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         private decimal TryParseDecimal(string value)
         {
             if (decimal.TryParse(value, out decimal result))
@@ -255,7 +300,96 @@ namespace FCF.TRIGGER_SAMPLE
             return doc.SelectSingleNode($"//FieldItem[@fieldId='{fieldId}']")?.Attributes["fieldValue"]?.Value;
         }
 
-        
+
         #endregion
+
+        #region 表單更新寫入的函數
+        /*
+         * TODO: 
+         * 1. 從資料庫中取得表單資料 XML 順序1
+         * 2. 將目標資料寫回 XML 順序3
+         * 3. 將 XML 寫回資料庫 順序2
+         */
+
+        /// <summary>
+        /// 從資料庫取得表單內容XML 透過TASK_ID
+        /// </summary>
+        /// <param name="taskId">表單TASK ID</param>
+        /// <returns>表單最新的XML</returns>
+        public XmlDocument GetFormDocByTaskID(string taskId)
+        {
+            const string sql = @"
+                SELECT TOP 1 [CURRENT_DOC] FROM [TB_WKF_TASK] WHERE [TASK_ID] = @taskId
+             ";
+            using (var connection = new System.Data.SqlClient.SqlConnection(_connectionString))
+            {
+                var docXml = connection.ExecuteScalar<string>(sql, new { taskId });
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(docXml);
+                return doc;
+            }
+        }
+        /// <summary>
+        /// 從資料庫取得表單內容XML 透過表單編號DOC_NBR
+        /// </summary>
+        /// <param name="DOC_NBR">表單編號 DOC_NBR</param>
+        /// <returns>表單最新的XML</returns>
+        public XmlDocument GetFormDocByDocNBR(string DOC_NBR)
+        {
+            const string sql = @"
+                SELECT TOP 1 [CURRENT_DOC] FROM [TB_WKF_TASK] WHERE [DOC_NBR] = @DOC_NBR
+             ";
+            using (var connection = new System.Data.SqlClient.SqlConnection(_connectionString))
+            {
+                var docXml = connection.ExecuteScalar<string>(sql, new { DOC_NBR });
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(docXml);
+                return doc;
+            }
+        }
+
+        /// <summary>
+        /// 透過TASK_ID 更新表單內容XML
+        /// </summary>
+        /// <param name="taskId">taskID</param>
+        /// <param name="doc">表單XML</param>
+        public void SetFormDocByTaskId(string taskId, XmlDocument doc)
+        {
+            //Ede.Uof.Utility.Log.Logger.Write("Debug", $"doc = {doc.OuterXml}");
+            //將XML轉為字串
+            string docXml = doc.OuterXml;
+            const string sql = @"
+                UPDATE [TB_WKF_TASK] 
+                SET [CURRENT_DOC] = @docXml 
+                WHERE [TASK_ID] = @taskId
+             ";
+            using (var connection = new System.Data.SqlClient.SqlConnection(_connectionString))
+            {
+                connection.Execute(sql, new { docXml, taskId });
+            }
+        }
+
+        /// <summary>
+        /// 更新表單欄位
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <param name="fieldId"></param>
+        /// <param name="fieldValue"></param>
+        public void ReplaceDocumentContent(string taskId,string fieldId,string fieldValue)
+        {
+            //取得表單內容
+            var doc = GetFormDocByTaskID(taskId);
+            //更新XML的內容
+            XmlNode node = doc.SelectSingleNode($"//Form/FormFieldValue/FieldItem[@fieldId='{fieldId}']");
+            //更新node 的fieldValue=
+            if(node != null)
+            {
+                node.Attributes["fieldValue"].Value = fieldValue;
+                //回存
+                SetFormDocByTaskId(taskId, doc);
+            }                        
+        }
+
+        #endregion 表單更新寫入的函數
     }
 }
